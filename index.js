c227a9d6fa569f01aed23be45a839cc132439862
saptarshi chakraborty <saptarshi34.sc@gmail.com>
2022-06-07 15:59:29 +0530

    commit-v1

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const genres = [
 app.get('/', (req, res) => {
     res.send('welcome to Vidly')
 })
-//GET:://
+//GET::
 app.get('/api/genres', (req, res) => {
     res.send(genres)
 })
@@ -20,11 +20,12 @@ app.get('/api/genres/:id', (req, res) => {
     const genre = genres.find(c => c.id === parseInt(req.params.id))
     if (!genre) {
         res.status(404).send('ID is not present')
+        return
     } else {
         res.send(genre)
     }
 })
-//POST:://
+//POST::
 app.post('/api/genres', (req, res) => {
     const schema = Joi.object({
         name: Joi.string().min(3)
@@ -32,9 +33,10 @@ app.post('/api/genres', (req, res) => {
             .required()
     })
 
-    const validation = schema.validate(req.body);
-    if (validation.error) {
-        res.status(400).send(validation.error.details[0].message)
+    const {error} = validateGenres(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message)
+        return
     }
     const genre = {
         id: genres.length + 1,
@@ -43,7 +45,48 @@ app.post('/api/genres', (req, res) => {
     genres.push(genre)
     res.send(genre)
 })
+//PUT::
 
+app.put('/api/genres/:id',(req,res) => {
+   const genre = genres.find(c => c.id === parseInt(req.params.id))
+   if(!genre){
+       res.status(404).send('doc-id is not present.')
+   }
+   //validation
+   const { error }  = validateGenres(req.body)
+   if(error) {
+    res.status(400).send(error.details[0].message)
+    return;
+}
+   genre.name = req.body.name
+   res.send(genre)
+
+})
+
+
+//DELETE::
+app.delete('/api/genres/:id',(req,res) =>{
+   const genre = genres.find(c => c.id === parseInt(req.params.id))
+   if(!genre){
+      res.status(400).send('ID is not present')
+      return
+   }
+   const index = genres.indexOf(genre)
+   genres.splice(index,1)
+   res.send(genres)
+})
+
+//validation::
+
+function validateGenres(genre){
+const schema = Joi.object({
+    name:Joi.string().min(3).max(100).required()
+})
+return schema.validate(genre)
+
+}
+
+//set env varibale for port
 const port = process.env.PORT || 7000
 app.listen(port, () => {
     console.log(`your app is listening ${port}`)
